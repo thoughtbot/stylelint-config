6b6c1f145d23a59e491f0ae1531bd3b45b851cbc
Tyson Gach <tyson@tysongach.com>
2019-07-08 14:50:24 -0400

    Enable scss/map-keys-quotes rule
    Require quoted keys in Sass maps.
    
    Documentation
    https://github.com/kristerkari/stylelint-scss/blob/master/src/rules/map-keys-quotes/README.md

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ module.exports = {
     "scss/at-rule-no-unknown": true,
     "scss/dollar-variable-colon-space-after": "always",
     "scss/dollar-variable-colon-space-before": "never",
+    "scss/map-keys-quotes": "always",
     "scss/no-duplicate-dollar-variables": true,
     "scss/operator-no-unspaced": true,
     "scss/selector-no-redundant-nesting-selector": true,
